4bd691bad68362575a1c292850d4815e28da76f8
Steve Mobley <steve@m77.us>
2019-10-24 20:24:19 -0400

    Add link time check if no RF time
    Rearranged and add allowance for no RF record.
    
    
    WIP
    
    
    WIP
    
    
    WIP
    
    Update readme

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
-ReflectorUnlink
+# ReflectorUnlink
+
+These script files work together to help manage DStar reflector linking and
+unlinking on a pi-star system.
+
+checkLink.sh should be run on a cron job periodically. It will examine the
+pi-star logs and determine if it is time to disconnect the reflector. It uses
+multiple criteria to determine if it should disconnect, as listed here:
+    - If there is no link active, nothing will happen.
+    - If the flag indicating the link was made by netLink.sh, nothing will
+        happen.
+    - If the above criteria are met:
+        - Get last link time.
+        - Get last RF activity time, if any.
+        - Set delta time to now minus least of last link time and last RF time.
+        - If delta time is greater than inactivity time, then disconnect
+            from reflector
+
+I have these script files in /home/pi-star/bin but you can put them elsewhere
+if you wish. Just make sure you set your cron jobs to the proper location.
+As you can see in checkLink.sh, I have mine set to 3600 seconds (one hour)
+of inactivity. I have it run via crontab every five minutes. Also note you
+need to put in the callsign for your repeater or hotspot. When putting these
+files on your pi-star, you will need to issue rpi-rw to remount the filesystem
+as writable. I use putty to connect for this and  winscp to copy the files to
+the proper folders. You'll also need putty or something similar to setup
+your cron jobs (sudo crontab -e).
+
+netLink.sh can be used to make timed connections to reflectors for scheduled
+nets. Since it calls the pistar-link script, it takes the same arguments and
+passes them on. The format is ref030_b for reflector 30 B. Also unlink is used
+to unlink the reflector. netLink.sh simply adds the setting and clearing of
+/tmp/netLink.flag that is used by checkLink.sh to determine if this is a
+scheduled link for nets and does not do the disconnect on inactivity if so.
+
+Here is what I have in my root's crontab:
+
+```bash
+## Check for idle reflectors linked
+*/5 * * * * /home/pi-star/bin/checkLink.sh
+
+## SE DStar weather net
+55 20 * * 0 /home/pi-star/bin/netLink.sh ref004_a fixed
+00 22 * * 0 /home/pi-star/bin/netLink.sh unlink
+
+## Ham Nation after show net
+45 21 * * 3 /home/pi-star/bin/netLink.sh ref014_c fixed
+59 23 * * 3 /home/pi-star/bin/netLink.sh unlink
+```
+
+Hope you find this useful. 
+Feel free to give feedback, good, bad or suggestions.
+
+Steve - WB4BXO
+www.wb4bxo.us
--- checkLink.sh
+++ checkLink.sh
@@ -14,49 +14,67 @@ INACTIVITY_TIME="3600"
 
 ##### Only test/disconnect if not linked for a net by netLink.sh
 # This means you must explicitely unlink after a net using netLink.sh
+# which sets and clears this flag.
 if [ ! -f /tmp/netLink.flag ]; then
-
   ######################################################################
-  #get last RF DStar header (same as last transmit to repeater) 
-  LAST_RF=$(grep "Repeater header" /var/log/pi-star/Headers.log | tail -n 1)
-  echo $LAST_RF
+  # Get the Links.log to see if and when we connected to the reflector.
+  # One reason is so we won't disconnect if not connected, another reason
+  # is to see if we connected via the control channel instead of RF and 
+  # use that time instead.
+  LINKS_LOG=$(cat /var/log/pi-star/Links.log)
+  if [ ${#LINKS_LOG} != 0 ]; then
+    ######################################################################
+    #get last RF DStar header (same as last transmit to repeater) 
+    LAST_RF=$(grep "Repeater header" /var/log/pi-star/Headers.log | tail -n 1)
+    LAST_RF=${LAST_RF:=0}
+    echo $LAST_RF
 
-  # And munge it a bit to get specific info fields
-  TIME_STAMP=${LAST_RF%: Repeater*}
-  echo [$TIME_STAMP]
-  TIME_THEN=$(date -u -d "${TIME_STAMP}" +"%s")
-  echo [$TIME_THEN]
-  TIME_NOW=$(date +"%s")
-  echo [$TIME_NOW]
-  TIME_DELTA=$(($TIME_NOW-$TIME_THEN))
-  echo [$TIME_DELTA]
+    # Munge a bit to get times
+    TIME_STAMP=${LAST_RF%: Repeater*}
+    echo [$TIME_STAMP]
+    TIME_T_THEN=$(date -u -d "${TIME_STAMP}" +"%s")
+    echo [$TIME_T_THEN]
+    TIME_T_NOW=$(date +"%s")
+    echo [$TIME_T_NOW]
+    TIME_T_DELTA=$(($TIME_T_NOW-$TIME_T_THEN))
+    TIME_CONNECT=${LINKS_LOG%: *Type:*}
+    TIME_T_LINK=$(date -u -d "${TIME_CONNECT}" +"%s")
+    USING_LINK_TIME=false
+    if (($TIME_T_LINK>$TIME_T_THEN)); then
+      TIME_T_DELTA=$(($TIME_T_NOW-$TIME_T_LINK))
+      USING_LINK_TIME=true
+    fi
+    #TIME_T_DELTA now has the elapsed time since either last RF or LINK time,
+    # whichever is shorter.
+    echo [$TIME_T_DELTA]
 
-  MY_CALL=$(echo ${LAST_RF#*My: })
-  MY_CALL=${MY_CALL% Your:*}
-  echo [$MY_CALL]
+    MY_CALL=$(echo ${LAST_RF#*My: })
+    MY_CALL=${MY_CALL% Your:*}
+    echo [$MY_CALL]
 
-  UR_CALL=$(echo ${LAST_RF#*Your: })
-  UR_CALL=${UR_CALL% Rpt1:*}
-  echo [$UR_CALL]
+    UR_CALL=$(echo ${LAST_RF#*Your: })
+    UR_CALL=${UR_CALL% Rpt1:*}
+    echo [$UR_CALL]
 
-  RPT1_CALL=$(echo ${LAST_RF#*Rpt1: })
-  RPT1_CALL=${RPT1_CALL% Rpt2:*}
-  echo [$RPT1_CALL]
+    RPT1_CALL=$(echo ${LAST_RF#*Rpt1: })
+    RPT1_CALL=${RPT1_CALL% Rpt2:*}
+    echo [$RPT1_CALL]
 
-  RPT2_CALL=$(echo ${LAST_RF#*Rpt2: })
-  RPT2_CALL=${RPT2_CALL% Flags:*}
-  echo [$RPT2_CALL]
+    RPT2_CALL=$(echo ${LAST_RF#*Rpt2: })
+    RPT2_CALL=${RPT2_CALL% Flags:*}
+    echo [$RPT2_CALL]
 
-  # check and see if it is a call routed through the gateway
-  if [ "$RPT2_CALL" == "${REPEATER_CALL} G" ]; then
-    if (($TIME_DELTA>$INACTIVITY_TIME)); then
-      # gateway call and it was longer ago than INACTIVITY_TIME
-      LINKS_LOG=$(cat /var/log/pi-star/Links.log)
-      if [ ${#LINKS_LOG} != 0 ]; then
-        # and it is currently linked
+    # check and see if it is a call routed through the gateway
+    if [ "$RPT2_CALL" == "${REPEATER_CALL} G" ] || $USING_LINK_TIME; then
+      if (($TIME_T_DELTA>$INACTIVITY_TIME)); then
+        # gateway call and it was longer ago than INACTIVITY_TIME
         /usr/local/sbin/pistar-link unlink
         echo "$(date)- Inactivity unlink" >>/var/log/pi-star/netLink.log
       fi
     fi
+  else
+    echo "Not currently linked!"
   fi
+else
+  echo "Flagged as a Net link, don't disconnect on inactivity!"
 fi
